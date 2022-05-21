9250e461a66c6a716c41f28475e3c3e85987c6e6
Steve Dakh <slavik0329@gmail.com>
2022-05-21 18:48:55 -0400

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ function App() {
           const decoded = findContent($, 'txt');
           console.log('tx', decoded.content)
 
-          chrome.tabs.create({ url: `http://localhost:3000/?content=${encodeURIComponent(decoded.content)}` });
+          chrome.tabs.create({ url: `https://halfbold.com/?content=${encodeURIComponent(decoded.content)}` });
         }
       });
     });
@@ -32,20 +32,7 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo"/>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
